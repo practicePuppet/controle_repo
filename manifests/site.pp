55705af85b6c3848e9cb7f97af82ad562a090196
practicePuppet <75258589+practicePuppet@users.noreply.github.com>
2020-11-30 13:36:47 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'This is a README',
-    owner => 'root',
-  }
 }
 node 'localhost.localdomain' {
   include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => $fqdn,
+    owner => 'root',
+  }
 }
 
 node /^web/ {
